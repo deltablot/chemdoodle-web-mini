185d872f541eefea12ef37cd6f4897cbc7682040
Nicolas CARPi <nico-git@deltablot.email>
2021-05-30 15:16:49 +0200

    init

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/**
+ * Gruntfile.js
+ *
+ * Run 'grunt' in shell to minify the files
+ *
+ */
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+          banner: '/*! <%= pkg.name %> <%= pkg.homepage %> */\n',
+        mangle: false
+      },
+      dist: {
+        files: {
+          'dist/chemdoodle.min.js': [
+              'src/chemdoodle-unpacked.js',
+              'src/chemdoodle-uis-unpacked.js'],
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+
+  // before minifying js it is preferable to do 'yarn install' to update the dependencies
+  grunt.registerTask('default', ['uglify']);
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# chemdoodle-web-mini
+
+ChemDoodle Web Components, minified.
+
+The idea behind this lib is to be able to get the chemdoodle files from npm.
+
+# Usage
+
+Copy the minified file from `dist/` into your project's assets folder.
